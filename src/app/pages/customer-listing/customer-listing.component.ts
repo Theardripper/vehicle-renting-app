80d0ac1a682a06ffb4d4412a0bb4e76623fca81f
Theardripper <eduardomoura512@outlook.com>
2026-09-23 18:22:44 -0300

    inital commit

---
+++ src/app/pages/customer-listing/customer-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-listing.component.html',
+  styleUrl: './customer-listing.component.scss'
+})
+export class CustomerListingComponent {
+
+}
---
+++ src/app/pages/vehicle-master/vehicle-master.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-master',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicle-master.component.html',
+  styleUrl: './vehicle-master.component.scss'
+})
+export class VehicleMasterComponent {
+
+}
